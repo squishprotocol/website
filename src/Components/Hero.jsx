6f047f3bd81158decb434be1ceb28e107c6d1188
squish protocol <130642569+squishprotocol@users.noreply.github.com>
2023-04-29 14:48:04 +0530

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import FlipCounter from "../Components/FlipCounter"
+
 import logo from "../../public/logo.png"
 const Hero = () => {
     return (
         <div>
-            <section><FlipCounter/>
+            <section>
                 <div className="grid h-screen px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                     <div className="mr-auto place-self-center lg:col-span-7">
                         <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Stake Your $SQUISH and earn BNB,BUSD and more</h1>
