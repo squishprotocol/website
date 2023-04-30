a0416315f2c722381f567aaf65b75e11a8890714
squish protocol <130642569+squishprotocol@users.noreply.github.com>
2023-04-30 11:09:11 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
       <div className="p-2 border border-red-400 text-red-400"><a href="https://t.me/squishprotocol">JOIN TELEGRAM</a></div>
     </div>
     <div className="dropdown dropdown-end">
-      <div className="p-2 border border-red-400 text-red-400">Connect</div>
+      <div className="p-2 border border-red-400 text-red-400"><a href="https://twitter.com/intent/tweet?text=$sq%20Fair%20Launch%0ALP%20BURN,%20No%20minting,%20STAKING%20REWARD.%0ATotal%20supply%20200000%20$SQ.%0A%0ACA:%200xcfa661975D9b3f6bc7fcb60642c366A12E856348%0A&url=https://dexscreener.com/bsc/0x3b121b20e877366d33c0368be15485b418be786e/%0A&hashtags=musk,bscgem,SQ,crypto,pepe,squish,1000xgem">shill now</a></div>
     </div>
   </div>
 </div>
